ff7de1e3f985d4b6313994596351439be63231ac
Amogh Bhagwat <amoghbhagwat00@gmail.com>
2020-04-08 16:15:16 +0530

    Added filename parameter to saver service

--- src/image_saver.cpp
+++ src/image_saver.cpp
@@ -4,24 +4,39 @@
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
 #include <opencv2/highgui/highgui.hpp>
+#include <boost/format.hpp>
 
 using namespace cv;
 
 bool save_image;
-std::string path, filename;
+int count;
+boost::format g_format;
 
 bool capture(std_srvs::Empty::Request &req, std_srvs::Empty::Response &resp){
   save_image = true;
+  count = count + 1;
   return true;
 }
 
 void webcam_callback(const sensor_msgs::ImageConstPtr &image){
   cv_bridge::CvImagePtr cv_ptr;
   cv_ptr = cv_bridge::toCvCopy(image, sensor_msgs::image_encodings::BGR8);
+  std::string filename_;
+  // setting the count and foramt of the image if not set by user
+  if (!cv_ptr->image.empty()){
+    try{
+      filename_ = (g_format).str();
+    } catch (...) {g_format.clear();}
+    try{
+      filename_ = (g_format % count).str();
+    } catch (...) {g_format.clear();}
+    try{
+      filename_ = (g_format % count % "jpg").str();
+    } catch (...) {g_format.clear();}
+  }
   if (save_image == true){
-    filename = path + image->header.frame_id + ".jpg";
-    imwrite(filename, cv_ptr->image);
-    ROS_INFO("Saved Image %s", filename.c_str());
+    imwrite(filename_, cv_ptr->image);
+    ROS_INFO("Saved Image %s", filename_.c_str());
     save_image = false;
   }
 }
@@ -29,12 +44,16 @@ void webcam_callback(const sensor_msgs::ImageConstPtr &image){
 int main (int argc, char **argv){
   ros::init(argc, argv, "image_saver_server");
   ros::NodeHandle nh;
+  ros::NodeHandle private_nh("~");
   // set subscriber for video and service for saving images
   ros::Subscriber video_sub = nh.subscribe<sensor_msgs::Image>("/webcam", 1, webcam_callback);
   ros::ServiceServer service = nh.advertiseService("image_saver", capture);
   // initialze save_image and filename variable
   save_image = false;
-  path = "/home/amogh/images/";
+  count = 0;
+  std::string filename;
+  private_nh.param<std::string>("filename", filename, std::string("left%04i.%s"));
+  g_format.parse(filename);
   ROS_INFO("Ready to capture image");
 
   ros::spin();
